3bbf468a06d6026fbf057818737be126221e55b5
Brenda <brendamitto@gmail.com>
2025-11-27 16:28:41 -0300

    Feat- Criacao CustomDrawer

---
+++ SistemaAdocaoAppreactnative/components/CustomDrawerContent.tsx
@@ -0,0 +1,44 @@
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const CustomDrawerContent = (props: any) => {
+  return (
+    <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
+      <View style={styles.header}>
+        <Image
+          source={require('../assets/images/cachorro.png')} 
+          style={styles.avatar}
+        />
+        <Text style={styles.name}>Sistema Adoção</Text>
+      </View>
+      <View style={{ flex: 1, paddingTop: 10 }}>
+        <DrawerItemList {...props} />
+      </View>
+    </DrawerContentScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    backgroundColor: '#3F51B5', 
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+    borderWidth: 2,
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+  },
+  name: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
+
+export default CustomDrawerContent;
